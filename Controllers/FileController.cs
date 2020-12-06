9a4baf68223160fd86a48863bd8f3da0f170ab11
Jelle Bootsma <jellehb@gmail.com>
2020-12-06 18:05:31 +0100

    Started on file management

---
+++ Controllers/FileController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RWFM.Repositories;
+using RWFM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RWFM.Controllers
+{
+    [VerifyUser]
+    public class FileController : Controller
+    {
+        private readonly ILogger<FileController> _logger;
+
+        public FileController(ILogger<FileController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult GetRootPath()
+        {
+            return new JsonResult(new { path = "C://" });
+        }
+
+        public IActionResult GetFolderContent(string path)
+        {
+            FolderModel folder = new FolderModel(path);
+            return new JsonResult(folder.GetChildFiles());
+        }
+    }
+}
---
+++ Models/FolderModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RWFM.Models
+{
+    public class FolderModel
+    {
+        private string absolutePath;
+        private List<string> childFiles;
+        private List<FolderModel> childFolders;
+        private bool loaded = false;
+
+        public FolderModel(string path)
+        {
+            absolutePath = path;
+        }
+
+        public List<FolderModel> GetChildFolders()
+        {
+            if (!IsLoaded())
+            {
+                LoadChildren();
+            }
+            return childFolders;
+        }
+        public List<string> GetChildFiles()
+        {
+            if (!IsLoaded())
+            {
+                LoadChildren();
+            }
+            return childFiles;
+        }
+
+        public bool IsLoaded()
+        {
+            return loaded;
+        }
+
+        public void LoadChildren()
+        {
+            if (Directory.Exists(absolutePath))
+            {
+                childFiles = Directory.GetFiles(absolutePath).ToList<string>();
+                var directories = Directory.GetDirectories(absolutePath);
+                childFolders = new List<FolderModel>();
+                foreach (string directory in directories)
+                {
+                    childFolders.Add(new FolderModel(directory));
+                }
+                loaded = true;
+            }
+        }
+    }
+}
